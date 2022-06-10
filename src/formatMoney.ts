25c39a2f962932a6799c80bd8c383ae7b4fc23fa
g0blin <742005790@qq.com>
2022-06-10 20:46:21 +0800

    feat: 添加了币种符号

--- src/formatMoney.ts
+++ src/formatMoney.ts
@@ -1,17 +1,13 @@
 /**
  * 格式化金钱文本，隔三位加逗号
- * @param amount 金钱数字
- * @param digit 小数位数
+ * @param {number} amount 金钱数字
+ * @param {number} digit 小数位数
+ * @param {string} symbol 币种符号
  * @returns {string} 格式化后的金钱文本
  * @example
- * formatMoney();   // '0.00'
- * formatMoney(99, 1);  // '99.0'
- * formatMoney(1999);   // '1,999.00'
- * formatMoney(100000000);  // '100,000,000.00'
+ * formatMoney(100, 2, '$'); // $100.00
  */
-function formatMoney(amount: number, digit?: number): string {
-    amount = amount || 0;
-    if (digit === undefined) digit = 2;
+function formatMoney(amount: number = 0, digit: number = 2, symbol?: string): string {
     const amountArr = String(amount.toFixed(digit)).split('.');
     const int = amountArr[0];
     const decimal = amountArr[1] || '';
@@ -23,7 +19,7 @@ function formatMoney(amount: number, digit?: number): string {
         spStr += i === 0 && (spStr === '-' || !spStr) ? int.substr(spIdx, 3) : `,${int.substr(spIdx, 3)}`;
         spIdx += 3;
     }
-    return decimal ? `${spStr}.${decimal}` : spStr;
+    return decimal ? `${symbol ? symbol : ''}${spStr}.${decimal}` : `${symbol ? symbol : ''}${spStr}`;
 }
 
 export default formatMoney;
